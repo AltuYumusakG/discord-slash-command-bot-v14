f2956d937f7e96df3694c7781f7f04e49bdf9ca7
Altuğ <altugkisisel@gmail.com>
2022-12-30 23:50:59 +0300

    Updated ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
-        let activities = [`${client.user.username}`, `${client.guilds.cache.size.toString()} Server(s))`], i = 0;
+        let activities = [`${client.user.username}`, `${client.guilds.cache.size.toString()} Server(s)`], i = 0;
         setInterval(() => client.user.setActivity({ name: `${activities[i++ % activities.length]}`, type: ActivityType.Listening }), 30 * 1000);
     }
 };
